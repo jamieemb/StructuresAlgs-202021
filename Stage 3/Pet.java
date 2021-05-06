9b3408077337455aac2ed293be0a74d58d4dc59a
Jamie Embleton <77155482+jamieemb@users.noreply.github.com>
2021-05-06 15:51:25 +0100

    Add files via upload

---
+++ Stage 3/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+
+    SimpleLinkedList products;
+
+    public Pet() {
+        this.products = new SimpleLinkedList();
+    }
+
+    public SimpleLinkedList getProducts() {
+        return products;
+    }
+}
